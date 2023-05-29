1ec24f017a98dcfd136a3768491a894d8ff30901
VAILLANT Romain <4906KW@tera.infragaz.com>
2023-05-29 23:26:32 +0200

    streamlit folium

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,7 +11,7 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 import plotly.express as px
 from folium.plugins import HeatMap
-from streamlit_folium import st_folium
+from streamlit-folium import st_folium
 
 
 
