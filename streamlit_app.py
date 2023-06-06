13858ff577862b4cbe9de1c34c63415acb90a2f4
VAILLANT Romain <4906KW@tera.infragaz.com>
2023-06-06 09:38:50 +0200

    modified columns layout

--- streamlit_app.py
+++ streamlit_app.py
@@ -44,8 +44,9 @@ def main():
     )
 
     # Layout
-    col1, col2, col3 = st.columns(3)
+    col1, col2 = st.columns([2,1])
     with col1:
+        
         st.subheader("1. Nombre de sites mis en service")
 
         # Prepare data
@@ -83,13 +84,101 @@ def main():
         # 2. Map Plot
         st.map(df_sample)
 
-    with col3:
+    #
+    col1 = st.columns(1)
+
+    st.subheader("4. Production nationale horaire")
+
+    # Prepare Data
+    df_horaire[["annee", "mois", "jour"]] = df_horaire["Journée gazière"].str.split(
+        "-", expand=True
+    )
+
+    #
+    heures_liste = [
+        "0" + str(i) + ":00" if i < 10 else str(i) + ":00" for i in range(24)
+    ]
+
+    col1_bis, col2_bis, col3_bis, col4_bis = st.columns(4)
+
+    with col1_bis:
+        # streamlit component
+        date_1 = st.date_input(
+            "Sélectionner une date de début:", datetime.date(2022, 1, 1)
+        )
+        # streamlit component
+        h_min = st.selectbox(
+            "Sélectionner une heure de début:", (heures_liste), index=6
+        )
+
+    with col2_bis:
+        # streamlit component
+        date_2 = st.date_input(
+            "Sélectionner une date de fin:", datetime.date(2022, 1, 21)
+        )
+        # streamlit component
+        h_max = st.selectbox(
+            "Sélectionner une heure de fin:", (heures_liste), index=12
+        )
+
+    # Prepare data
+    month_1 = "0" + str(date_1.month) if date_1.month < 10 else str(date_1.month)
+    month_2 = "0" + str(date_2.month) if date_2.month < 10 else str(date_2.month)
+
+    day_1 = "0" + str(date_1.day) if date_1.day < 10 else str(date_1.day)
+    day_2 = "0" + str(date_2.day) if date_2.day < 10 else str(date_2.day)
+
+    df_horaire_week = df_horaire.loc[
+        (df_horaire["annee"] >= str(date_1.year))
+        & (df_horaire["annee"] <= str(date_2.year))
+        & (df_horaire["mois"] >= month_1)
+        & (df_horaire["mois"] <= month_2)
+        & (df_horaire["jour"] >= day_1)
+        & (df_horaire["jour"] <= day_2)
+    ]
+
+    df_horaire_week = (
+        df_horaire_week.drop(
+            columns=[
+                "id",
+                "Annee/Mois",
+                #'Journée gazière',
+                "Production Journalière (MWh PCS)",
+                "Opérateur",
+                "Nombre de sites d'injection raccordés au réseau de transport",
+                "Statut de la donnée",
+                "annee",
+                "mois",
+                "jour",
+            ]
+        )
+        .set_index("Journée gazière")
+        .loc[:, h_min:h_max]
+    )
+
+    # 4. Bar Plot
+    fig = px.bar(
+        df_horaire_week,
+        labels={
+            "value": "Production (MWh PCS)",
+            "variable": "Heure",
+        },
+        title=("Production Horaire Nationale"),
+    )
+
+    # Show
+    st.plotly_chart(fig, use_container_width=True)
+
+    col1, col2 = st.columns([1,2])
+
+    with col1:
+        
         st.subheader("3. Sites par capacité de production")
 
         # streamlit component
         top_value = st.slider("Top", 0, 30, 10)
 
-        # Prepare data
+    #     # Prepare data
         df_pts_bar = (
             df_pts[["Nom du site", "Capacite de production (GWh/an)"]]
             .sort_values("Capacite de production (GWh/an)", ascending=False)
@@ -107,92 +196,6 @@ def main():
             orientation="h",
         )
 
-        st.plotly_chart(fig, use_container_width=True)
-
-    #
-    col1, col2 = st.columns(2)
-
-    with col1:
-        st.subheader("4. Production nationale horaire")
-
-        # Prepare Data
-        df_horaire[["annee", "mois", "jour"]] = df_horaire["Journée gazière"].str.split(
-            "-", expand=True
-        )
-
-        #
-        heures_liste = [
-            "0" + str(i) + ":00" if i < 10 else str(i) + ":00" for i in range(24)
-        ]
-
-        col1_bis, col2_bis, col3_bis, col4_bis = st.columns(4)
-
-        with col1_bis:
-            # streamlit component
-            date_1 = st.date_input(
-                "Sélectionner une date de début:", datetime.date(2022, 1, 1)
-            )
-            # streamlit component
-            h_min = st.selectbox(
-                "Sélectionner une heure de début:", (heures_liste), index=6
-            )
-
-        with col2_bis:
-            # streamlit component
-            date_2 = st.date_input(
-                "Sélectionner une date de fin:", datetime.date(2022, 1, 7)
-            )
-            # streamlit component
-            h_max = st.selectbox(
-                "Sélectionner une heure de fin:", (heures_liste), index=12
-            )
-
-        # Prepare data
-        month_1 = "0" + str(date_1.month) if date_1.month < 10 else str(date_1.month)
-        month_2 = "0" + str(date_2.month) if date_2.month < 10 else str(date_2.month)
-
-        day_1 = "0" + str(date_1.day) if date_1.day < 10 else str(date_1.day)
-        day_2 = "0" + str(date_2.day) if date_2.day < 10 else str(date_2.day)
-
-        df_horaire_week = df_horaire.loc[
-            (df_horaire["annee"] >= str(date_1.year))
-            & (df_horaire["annee"] <= str(date_2.year))
-            & (df_horaire["mois"] >= month_1)
-            & (df_horaire["mois"] <= month_2)
-            & (df_horaire["jour"] >= day_1)
-            & (df_horaire["jour"] <= day_2)
-        ]
-
-        df_horaire_week = (
-            df_horaire_week.drop(
-                columns=[
-                    "id",
-                    "Annee/Mois",
-                    #'Journée gazière',
-                    "Production Journalière (MWh PCS)",
-                    "Opérateur",
-                    "Nombre de sites d'injection raccordés au réseau de transport",
-                    "Statut de la donnée",
-                    "annee",
-                    "mois",
-                    "jour",
-                ]
-            )
-            .set_index("Journée gazière")
-            .loc[:, h_min:h_max]
-        )
-
-        # 4. Bar Plot
-        fig = px.bar(
-            df_horaire_week,
-            labels={
-                "value": "Production (MWh PCS)",
-                "variable": "Heure",
-            },
-            title=("Production Horaire Nationale"),
-        )
-
-        # Show
         st.plotly_chart(fig, use_container_width=True)
 
     with col2:
